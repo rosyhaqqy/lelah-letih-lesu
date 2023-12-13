073a8706a00a591ecb084518a24a6789ccd5e4b9
athraz <athrazan2004@gmail.com>
2023-12-13 12:00:49 +0700

    add no 2

--- bashrc/client/turkregion.sh
+++ bashrc/client/turkregion.sh
@@ -1,1 +1,8 @@
 echo nameserver 192.168.122.1 > /etc/resolv.conf
+
+apt-get update -y
+apt-get install netcat -y
+
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -p tcp -j DROP
+iptables -A INPUT -p udp -j DROP
